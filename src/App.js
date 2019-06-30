5750b0c8fe559b8f8cb128ac222b6b6d4555ec69
jimzers <adamleesaintfrancis@gmail.com>
2019-06-30 13:16:38 -0700

    got some stuff

--- src/App.js
+++ src/App.js
@@ -3,16 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 // import ReactWebRTC from './components/ReactWebRTC'
+function Chicken() {
+    return(
+        <div>
+        <h1>Demo</h1>
+        <h5>Powered by Agora and AWS. Custom models trained in AWS ML</h5>
+        <div className="panel-body">
+            App ID: <input id="appId" type="text" defaultValue="8f6a7a4e348c4e4f8d62344fbfba3010" size="36"/>
+            Channel: <input id="channel" type="text" defaultValue="1000" size="4"/>
+            Host: <input id="video" type="checkbox" checked/>
+        </div>
+
+        <div className="mb10">
+            <button id="trackbutton" className="bx--btn bx--btn--secondary" type="button" disabled>
+                Connect
+            </button>
+            <button id="nextimagebutton" className="mt10 bx--btn bx--btn--secondary" type="button" disabled>
+                Test Image
+            </button>
+            <div id="updatenote" className="updatenote mt10"> loading model ..</div>
+        </div>
+        <video className="videobox canvasbox" autoPlay="autoplay" id="myvideo" styles={{visibility: 'hidden'}}/>
+        <canvas id="canvas 2d" className="border canvasbox 2d"/>
+        <div className="mt10">
+            <img src="images/1.jpg" className="canvasbox  hidden" id="handimage" alt=""/>
+        </div>
+            <script src="../public/js/index.js"/>
+        </div>
+
+    );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Chicken/>
     </div>
   );
 }
